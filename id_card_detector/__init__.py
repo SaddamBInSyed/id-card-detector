1fe1a65081bc8ff013ac2122fb880f015e55fd9c
fcakyon <fcakyon@gmail.com>
2020-03-23 00:43:48 +0300

    release v0.1.1

--- id_card_detector/__init__.py
+++ id_card_detector/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from id_card_detector.cv_utils import (read_image,
                                        visualize_prediction,
